92e86533cb277bf9ff8bcf2aa680a4913eb74c81
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-12-25 10:11:12 +0500

    Update Commit

--- core/core/settings.py
+++ core/core/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -28,6 +29,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'root',
 ]
+
+JAZZMIN_UI_TWEAKS = {
+    "theme": "darkly"
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
